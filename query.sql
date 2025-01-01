2900b59047390477139c9ce29490ba929ed8a8cb
ayoub ben abdessadak <ayoub@crazy-it.nl>
2025-01-01 19:12:48 +0100

    deleted files

--- query.sql
+++
@@ -1,9 +0,0 @@
--- Showing indexes for aanmelding
-SHOW INDEX FROM Aanmelding;
-
--- Showing indexes for Artsen
-SHOW INDEX FROM Arts;
-
-SHOW TABLES;
-
-SELECT voornaam, achternaam, geslacht FROM Persoon WHERE geslacht='Man';
--- zorgverlener_heeft_team.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Zorgverlener_heeft_Team(Zorgverlener_big_code, Team_team_naam)
-VALUES
-("G24911A75", "Team Zorgzaam"),
-("21G1A5974", "Team Welzijn"),
-("A5172G149", "Team Vitaliteit"),
